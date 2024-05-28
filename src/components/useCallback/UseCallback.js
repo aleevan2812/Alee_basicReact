a90ccb0add20cffddf111d1319f2ce35511c1846
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-28 16:09:22 +0700

    final useCallback

--- src/components/useCallback/UseCallback.js
+++ src/components/useCallback/UseCallback.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import ChildComponent from "./ChildComponent";
 
 export const UseCallback = () => {
   const [users, setUsers] = useState([]);
 
-  const getData = (type) => {
+  const getData = useCallback((type) => {
     return fetch(`https://reqres.in/api/${type}`);
-  };
+  }, []);
 
+  // nham tranh cac useEffect o component con thuc thi lai khi nhan function tu component cha
+  // mat vung chua
   const handleClick = () => {
     getData("users")
       .then((res) => res.json())
